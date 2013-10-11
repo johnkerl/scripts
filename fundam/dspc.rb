7b4766fc744d401ee62252ff9ba507e1bde84b3a
John Kerl <kerl.john.r@gmail.com>
2013-10-10 21:11:30 -0400

    dspc

---
+++ fundam/dspc.rb
@@ -0,0 +1,19 @@
+#!/usr/bin/ruby
+$stdout.sync = true
+$stderr.sync = true
+
+# ================================================================
+# John Kerl
+# http://johnkerl.org
+# Double-spaces the input.
+# ================================================================
+
+ARGF.each do |line|
+	begin
+		puts
+	    puts line
+	rescue Errno::EPIPE
+	    # E.g. we're piped to head.  Don't complain.
+	    exit 0
+	end
+end
