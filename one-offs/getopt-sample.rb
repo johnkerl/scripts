e5f2916c382fd53ac0e23f0b32179762759c21d0
John Kerl <kerl.john.r@gmail.com>
2015-04-11 10:55:21 -0700

    ruby option-parser tweaks

--- one-offs/getopt-sample.rb
+++ one-offs/getopt-sample.rb
@@ -1,15 +1,17 @@
 #!/usr/bin/ruby
 
 # ================================================================
-# A ruby-getopt cheat sheet.
+# A Ruby option-parse cheat sheet.
 # ================================================================
+
+$us = File.basename $0
+$ourdir = File.dirname $0
 
 require 'getoptlong'
 
-# ----------------------------------------------------------------
 def usage()
-	$stderr.puts "Usage: #{File.basename $0} {-a aarg} {-f farg} {-i iarg} {-l}"
-	exit 1
+   $stderr.puts "Usage: #{File.basename $0} {-a aarg} {-f farg} {-i iarg} {-l}"
+   exit 1
 end
 
 # ----------------------------------------------------------------
@@ -28,13 +30,13 @@ opts = GetoptLong.new(
 
 begin
     opts.each do |opt, arg|
-	case opt
-		when '-a'; aarg = arg
-		when '-f'; farg = Float(arg)   # arg.to_f won't raise ArgumentError for invalid format
-		when '-i'; iarg = Integer(arg) # arg.to_i won't raise ArgumentError for invalid format
-		when '-l'; lopt = 1
-		when '--help'; usage
-	end
+   case opt
+      when '-a'; aarg = arg
+      when '-f'; farg = Float(arg)   # arg.to_f won't raise ArgumentError for invalid forma
+      when '-i'; iarg = Integer(arg) # arg.to_i won't raise ArgumentError for invalid forma
+      when '-l'; lopt = 1
+      when '--help'; usage
+   end
     end
 rescue GetoptLong::Error
     usage
@@ -49,5 +51,5 @@ puts "iarg = #{iarg}"
 puts "lopt = #{lopt}"
 puts "# non-option args: #{non_option_arg_count}"
 for arg in non_option_args
-	puts "Non-option arg: #{arg}"
+   puts "Non-option arg: #{arg}"
 end
