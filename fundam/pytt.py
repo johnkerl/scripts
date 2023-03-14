83081dd90133e5e901952e5d9c7a90bc5daa11a3
John Kerl <john.kerl@tiledb.com>
2023-03-14 14:41:37 +0000

    pytz continuation

--- fundam/pytt.py
+++ fundam/pytt.py
@@ -13,22 +13,19 @@ try:
     print('core version             ', ".".join(str(ijk) for ijk in list(tiledb.libtiledb.version())), tiledb.libtiledb.__file__)
 except ModuleNotFoundError:
     print("tiledb module not found")
-    sys.exit(1)
 
 # ----------------------------------------------------------------
 try:
     import tiledbsoma
     print('tiledbsoma.__version__   ', tiledbsoma.__version__, tiledbsoma.__file__)
+    soma = tiledbsoma
 except ModuleNotFoundError:
     print("tiledbsoma module not found")
-    sys.exit(1)
-soma = tiledbsoma
 
 try:
     import tiledbsoma.io
 except ModuleNotFoundError:
     print("tiledbsoma.io module not found")
-    sys.exit(1)
 
 # ----------------------------------------------------------------
 try:
@@ -44,12 +41,15 @@ if clib is not None:
     # Example:
     # 'libtiledbsoma=2be3147\nlibtiledb=2.12.2'
     lines = tiledbsoma.libtiledbsoma.version().split("\n")
-    for line in lines:
-        name, version = line.split("=")
-        path = ""
-        if name == "libtiledbsoma":
-            path = " " + tiledbsoma.libtiledbsoma.__file__
-        print("%-25s %s%s" % (name, version, path))
+    if len(lines) == 1:
+        print("%-25s %s%s" % ("libtiledbsoma", lines[0], tiledbsoma.libtiledbsoma.__file__))
+    else:
+        for line in lines:
+            name, version = line.split("=")
+            path = ""
+            if name == "libtiledbsoma":
+                path = " " + tiledbsoma.libtiledbsoma.__file__
+            print("%-25s %s%s" % (name, version, path))
 
 # ----------------------------------------------------------------
 try:
@@ -57,7 +57,6 @@ try:
     print('somacore.__version__     ', somacore.__version__, somacore.__file__)
 except ModuleNotFoundError:
     print("somacore module not found")
-    sys.exit(1)
 
 # ----------------------------------------------------------------
 try:
@@ -65,49 +64,42 @@ try:
     print('anndata.__version__  (ad)', ad.__version__)
 except ModuleNotFoundError:
     print("anndata module not found")
-    sys.exit(1)
 
 try:
     import numpy   as np
     print('numpy.__version__    (np)', np.__version__)
 except ModuleNotFoundError:
     print("numpy module not found")
-    sys.exit(1)
 
 try:
     import numba
     print('numba.__version__    (np)', numba.__version__)
 except ModuleNotFoundError:
     print("numba module not found")
-    sys.exit(1)
 
 try:
     import pandas  as pd
     print('pandas.__version__   (pd)', pd.__version__)
 except ModuleNotFoundError:
     print("pandas module not found")
-    sys.exit(1)
 
 try:
     import pyarrow as pa
     print('pyarrow.__version__  (pa)', pa.__version__)
 except ModuleNotFoundError:
     print("pyarrow module not found")
-    sys.exit(1)
 
 try:
     import scanpy  as sc
     print('scanpy.__version__   (sc)', sc.__version__)
 except ModuleNotFoundError:
     print("scanpy module not found")
-    sys.exit(1)
 
 try:
     import scipy   as sp
     print('scipy.__version__    (sp)', sp.__version__)
 except ModuleNotFoundError:
     print("scipy module not found")
-    sys.exit(1)
 
 print('python__version__        ', ".".join([str(e) for e in [sys.version_info.major, sys.version_info.minor, sys.version_info.micro]]))
 
