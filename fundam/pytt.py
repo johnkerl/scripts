2fb4bbc6dc0b64adba784a78d2182da891a4db79
John Kerl <kerl.john.r@gmail.com>
2024-11-14 22:50:53 +0000

    pytt.py catches

--- fundam/pytt.py
+++ fundam/pytt.py
@@ -14,22 +14,18 @@ try:
     t = tiledbsoma
     import tiledbsoma.io
     tio = tiledbsoma.io
-except (ModuleNotFoundError, AttributeError):
+except (ModuleNotFoundError, AttributeError, FileNotFoundError):
     print(">>>>>>>> tiledbsoma module not found")
 
 try:
     import tiledbsoma.io
-except (ModuleNotFoundError, AttributeError):
+except (ModuleNotFoundError, AttributeError, FileNotFoundError):
     print(">>>>>>>> tiledbsoma.io module not found")
 
 # ----------------------------------------------------------------
 try:
     import tiledbsoma.pytiledbsoma as clib
-except ModuleNotFoundError:
-    # main-old
-    clib = None
-except ImportError:
-    # main-old
+except (ModuleNotFoundError, AttributeError, FileNotFoundError):
     clib = None
 
 if clib is not None:
@@ -50,7 +46,7 @@ if clib is not None:
 try:
     import somacore
     print('somacore.__version__     ', somacore.__version__, somacore.__file__)
-except ModuleNotFoundError:
+except (ModuleNotFoundError, AttributeError, FileNotFoundError):
     print(">>>>>>>> somacore module not found")
 
 # ----------------------------------------------------------------
@@ -58,50 +54,50 @@ try:
     import tiledb
     print('tiledb.version           ', ".".join(str(e) for e in tiledb.version()), tiledb.__file__)
     print('core version             ', ".".join(str(ijk) for ijk in list(tiledb.libtiledb.version())), tiledb.libtiledb.__file__)
-except ModuleNotFoundError:
+except (ModuleNotFoundError, AttributeError, FileNotFoundError):
     print(">>>>>>>> tiledb module not found")
 
 # ----------------------------------------------------------------
 try:
     import anndata as ad
     print('anndata.__version__  (ad)', ad.__version__)
-except ModuleNotFoundError:
+except (ModuleNotFoundError, AttributeError, FileNotFoundError):
     print(">>>>>>>> anndata module not found")
 
 try:
     import numpy   as np
     print('numpy.__version__    (np)', np.__version__)
-except ModuleNotFoundError:
+except (ModuleNotFoundError, AttributeError, FileNotFoundError):
     print(">>>>>>>> numpy module not found")
 
 try:
     import numba
     print('numba.__version__    (np)', numba.__version__)
-except ModuleNotFoundError:
+except (ModuleNotFoundError, AttributeError, FileNotFoundError):
     print(">>>>>>>> numba module not found")
 
 try:
     import pandas  as pd
     print('pandas.__version__   (pd)', pd.__version__)
-except ModuleNotFoundError:
+except (ModuleNotFoundError, AttributeError, FileNotFoundError):
     print(">>>>>>>> pandas module not found")
 
 try:
     import pyarrow as pa
     print('pyarrow.__version__  (pa)', pa.__version__)
-except ModuleNotFoundError:
+except (ModuleNotFoundError, AttributeError, FileNotFoundError):
     print(">>>>>>>> pyarrow module not found")
 
 try:
     import scanpy  as sc
     print('scanpy.__version__   (sc)', sc.__version__)
-except ModuleNotFoundError:
+except (ModuleNotFoundError, AttributeError, FileNotFoundError):
     print(">>>>>>>> scanpy module not found")
 
 try:
     import scipy   as sp
     print('scipy.__version__    (sp)', sp.__version__)
-except ModuleNotFoundError:
+except (ModuleNotFoundError, AttributeError, FileNotFoundError):
     print(">>>>>>>> scipy module not found")
 
 print('python__version__        ', ".".join([str(e) for e in [sys.version_info.major, sys.version_info.minor, sys.version_info.micro]]))
@@ -117,24 +113,6 @@ def history(n=10):
     lines = [str(readline.get_history_item(i + 1)) for i in history_range]
     print('\n'.join(lines))
 # ----------------------------------------------------------------
-
-#ctx = tiledb.Ctx(
-#    {
-#        "sm.mem.total_budget": 4 * 1024 ** 3,
-#        "sm.consolidation.buffer_size": 512 * 1024 ** 2,
-#        # "sm.consolidation.mode": "fragments", # This is the default -- ?
-#        # "sm.consolidation.mode": "fragment_meta",
-#        "sm.consolidation.mode": "commits",
-#        # "sm.consolidation.mode": "array_meta",
-#    }
-#)
-
-ctx = tiledb.Ctx({
-    "py.init_buffer_bytes": 4 * 1024**3,
-    # "rest.use_refactored_array_open": True,
-    "sm.mem.reader.sparse_global_order.ratio_array_data": 0.3,
-})
-
 
 pass
 # Take further stuff from the '>>> ' prompt
