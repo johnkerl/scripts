3ae65f039b429a132eaa77d9004c798b388efda9
John Kerl <kerl.john.r@gmail.com>
2026-09-09 11:22:31 -0400

    Update py/pyt aliases

--- fundam/pyt.py
+++ fundam/pyt.py
@@ -6,19 +6,19 @@
 
 import os, sys, shutil, getopt, random, time, math, json
 
-import tiledb
+# import tiledb
 
-import numpy   as np
-import pandas  as pd
-import pyarrow as pa
-import anndata as ad
+# import numpy   as np
+# import pandas  as pd
+# import pyarrow as pa
+# import anndata as ad
 
-print('tiledb.version()         ', tiledb.version(), tiledb.__file__)
-print('core version             ', ".".join(str(ijk) for ijk in list(tiledb.libtiledb.version())), tiledb.libtiledb.__file__)
-print('numpy.__version__    (np)', np.__version__)
-print('pandas.__version__   (pd)', pd.__version__)
-print('pyarrow.__version__  (pa)', pa.__version__)
-print('python__version__        ', ".".join([str(e) for e in [sys.version_info.major, sys.version_info.minor, sys.version_info.micro]]))
+# print('tiledb.version()         ', tiledb.version(), tiledb.__file__)
+# print('core version             ', ".".join(str(ijk) for ijk in list(tiledb.libtiledb.version())), tiledb.libtiledb.__file__)
+# print('numpy.__version__    (np)', np.__version__)
+# print('pandas.__version__   (pd)', pd.__version__)
+# print('pyarrow.__version__  (pa)', pa.__version__)
+ print('python__version__        ', ".".join([str(e) for e in [sys.version_info.major, sys.version_info.minor, sys.version_info.micro]]))
 
 # ----------------------------------------------------------------
 import readline
